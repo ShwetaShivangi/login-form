82ca1d9df836824224ef44e866e2e10365032e45
saket <saketsa2.sn@gmail.com>
2017-03-01 09:41:08 +0530

    changes in login form

---
+++ app/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Dashboard () {
+  return (
+    <article>
+      <section className='text-section'>
+        <h1>Dashboard</h1>
+      </section>
+    </article>
+  )
+}
+
+export default Dashboard
